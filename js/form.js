c3ad2520806ff245d4f254b015439feb1f4187ba
Georg <georgiy.nikitin@gmail.com>
2020-02-12 20:02:35 +0300

    Добавлена валидация остальных полей

--- js/form.js
+++ js/form.js
@@ -1,29 +1,57 @@
 'use strict';
 
 (function () {
+  var NOT_FOR_GUESTS_ROOMS = 100;
+  var NO_GUESTS = 0;
+
+  var minAvailablePrice = {
+    bungalo: 0,
+    flat: 1000,
+    house: 5000,
+    palace: 10000
+  };
+
   var form = document.querySelector('.ad-form');
   var roomNumber = form.querySelector('#room_number');
   var capacityPeople = form.querySelector('#capacity');
+  var inputTitle = form.querySelector('#title');
+  var inputTypeHouse = form.querySelector('#type');
+  var inputPrice = form.querySelector('#price');
+  var inputTimeIn = form.querySelector('#timein');
+  var inputTimeOut = form.querySelector('#timeout');
 
-  // Проверка кастомной валидации на imput
+  // Проверка кастомной валидации на input
   form.addEventListener('change', function (evt) {
     evt.preventDefault();
     if (+roomNumber.value === 1 && +roomNumber.value !== +capacityPeople.value) {
       roomNumber.setCustomValidity('Допустимо 1 комната — «для 1 гостя»');
-    } else if (+roomNumber.value === 2 && (+roomNumber.value < +capacityPeople.value || +capacityPeople.value === 0)) {
+    } else if (+roomNumber.value === 2 && (+roomNumber.value < +capacityPeople.value || +capacityPeople.value === NO_GUESTS)) {
       roomNumber.setCustomValidity('Допустимо 2 комнаты — «для 2 гостей» или «для 1 гостя»');
-    } else if (+roomNumber.value === 3 && (+roomNumber.value < +capacityPeople.value || +capacityPeople.value === 0)) {
+    } else if (+roomNumber.value === 3 && (+roomNumber.value < +capacityPeople.value || +capacityPeople.value === NO_GUESTS)) {
       roomNumber.setCustomValidity('Допустимо 3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»');
-    } else if (+roomNumber.value === 100 && +capacityPeople.value !== 0) {
+    } else if (+roomNumber.value === NOT_FOR_GUESTS_ROOMS && +capacityPeople.value !== NO_GUESTS) {
       roomNumber.setCustomValidity('Допустимо 100 комнат — «не для гостей»');
     } else {
       roomNumber.setCustomValidity('');
     }
   });
 
+  // Проверка соответсвия типа жилья и цены
+  inputTypeHouse.addEventListener('input', function () {
+    inputPrice.placeholder = minAvailablePrice[inputTypeHouse.value];
+    inputPrice.min = minAvailablePrice[inputTypeHouse.value];
+  });
+
+  // Проверка соответсвия времени въезда выезда
+  inputTimeIn.addEventListener('input', function () {
+    inputTimeOut.value = inputTimeIn.value;
+  });
+
+  inputTimeOut.addEventListener('input', function () {
+    inputTimeIn.value = inputTimeOut.value;
+  });
 
   // Валидация Заголовка объявления
-  var inputTitle = form.querySelector('#title');
   inputTitle.addEventListener('input', function (evt) {
     evt.preventDefault();
     if (inputTitle.validity.tooShort) {
@@ -36,22 +64,6 @@
       inputTitle.setCustomValidity('');
     }
   });
-
-  // Валидация поля Цены за ночь
-  var inputPrice = form.querySelector('#price');
-  inputPrice.addEventListener('input', function (evt) {
-    evt.preventDefault();
-    if (inputPrice.validity.tooShort) {
-      inputPrice.setCustomValidity('Имя должно состоять минимум из 30-х символов, но не более 100-ти символов ');
-    } else if (inputPrice.validity.tooLong) {
-      inputPrice.setCustomValidity('Имя не должно превышать 100-ти символов');
-    } else if (inputPrice.validity.valueMissing) {
-      inputPrice.setCustomValidity('Обязательное поле для заполнения');
-    } else {
-      inputPrice.setCustomValidity('');
-    }
-  });
-
 
   inputTitle.value = 'Временно чтоб постоянно не забивать данные при проверки валидации';
 
