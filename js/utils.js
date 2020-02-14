4b79e2b04cabfbc5272dfcc0141d86743e0e04cb
Georg <georgiy.nikitin@gmail.com>
2020-02-14 18:06:47 +0300

    Удалил из js/data.js ненужные рандомные данные и перенес данные в js/data.js

--- js/utils.js
+++ js/utils.js
@@ -14,7 +14,7 @@
     },
     isEnterEvent: function (evt, action) {
       if (evt.keyCode === ENTER_KEYCODE) {
-        action();
+        action(evt);
       }
     },
     isMouseMainClickEvent: function (evt, action) {
@@ -24,15 +24,49 @@
     },
     // Показывает адрес текущей метки
     showAddress: function (x, y) {
+
       document.querySelector('#address').value = Math.round(x) + ', ' + Math.round(y);
     },
     // Спрятать элемент, добавив класс  'hidden'
     hideElement: function (hideClass) {
       document.querySelector(hideClass).classList.add('hidden');
     },
+
     // Показать элемент, убрав класс  'hidden'
     showElement: function (showClass) {
       document.querySelector(showClass).classList.remove('hidden');
-    }
+    },
+
+    // Выдает рандомное число в диапозоне от minNumber до maxNumber
+    getRandomRange: function (minNumber, maxNumber) {
+      return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    },
+
+    // Выдает на основе входящего массива один рандомный элемент массива
+    getRandomElement: function (arr) {
+      var numberRandom = window.utils.getRandomRange(0, arr.length - 1);
+      return arr[numberRandom];
+    },
+
+    // Выдает на основе входящего массива массив с рандомным кол-вом элементов
+    getRandomArray: function (arr) {
+      var numberRandom = window.utils.getRandomRange(0, arr.length);
+      var arrClon = arr.slice();
+      var arrNew = [];
+      var numberArrRandom;
+
+      for (var j = 0; j < numberRandom; j++) {
+        numberArrRandom = window.utils.getRandomRange(0, arrClon.length - 1);
+        arrNew[j] = arrClon[numberArrRandom];
+        arrClon.splice(numberArrRandom, 1);
+      }
+      return arrNew;
+    },
+
+    // Выдает рандомный ключ объекта
+    getRandomObject: function (obj) {
+      var array = Object.keys(obj);
+      return window.utils.getRandomElement(array);
+    },
   };
 })();
