798e38fd3f5ec7094b52dc7e0e474ab5b36e7bcd
Georg <georgiy.nikitin@gmail.com>
2020-02-18 10:52:00 +0300

    Удалил ненужные комментарии

--- js/filter.js
+++ js/filter.js
@@ -5,7 +5,7 @@
   var filterTypeOfHouse = filters.querySelector('#housing-type');
   var addressDataCopy = [];
 
-
+  // Фильтрация по типу жилья
   filterTypeOfHouse.addEventListener('change', function () {
     if (window.map.openCardStatus) {
       window.map.closePopup();
@@ -22,29 +22,5 @@
     window.pin.renderPins(addressDataCopy.slice(0, 5));
 
   });
-  /*
-  // console.log('статус', window.map.openCardStatus);
 
-  // Фильтр по типу жилья
-  filterTypeOfHouse.addEventListener('change', function () {
-    if (window.map.openCardStatus) {
-      window.map.closePopup();
-    }
-
-    addressDataCopy = [];
-    if (filterTypeOfHouse.value === 'any') {
-      addressDataCopy = window.map.addressData;
-    } else {
-      addressDataCopy = window.map.addressData.filter(function (data) {
-
-        return data.offer.type === filterTypeOfHouse.value;
-      });
-    }
-
-    console.log('Массив после фильтрации', addressDataCopy, addressDataCopy.slice(0, 1));
-    window.pin.renderPins(addressDataCopy);
-
-
-  });
-*/
 })();
