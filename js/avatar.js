c9b53374a7c6e860161dfb12e18cccc8cc377226
Georg <georgiy.nikitin@gmail.com>
2020-02-19 12:29:20 +0300

    Подключение модуля загрузки фото avatar

---
+++ js/avatar.js
@@ -0,0 +1,54 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var avatarChooser = document.querySelector('.ad-form__field input[type=file]');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+
+  avatarChooser.addEventListener('change', function () {
+    var file = avatarChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatarPreview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  var fileChooser = document.querySelector('.ad-form__uploud input[type=file]');
+  var preview = document.querySelector('.ad-form__photo');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+      var newElementImg;
+
+      reader.addEventListener('load', function () {
+        newElementImg = '<img alt="Фотография жилья" width="70" height="70">';
+        preview.appendChild(newElementImg);
+        preview.querySelector('img').src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+
+})();
