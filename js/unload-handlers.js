0694e30cb1e706f868b739134d35e30c79887f30
Georg <georgiy.nikitin@gmail.com>
2020-02-21 13:33:47 +0300

    Вынес обработку отправки формы в отдельный модуль

---
+++ js/unload-handlers.js
@@ -0,0 +1,92 @@
+'use strict';
+// Обработка успешной и неуспешной отправки формы
+(function () {
+  var main = document.querySelector('main');
+
+  // Закрытие окна успешной загрузки
+  var closePopupSuccessLoad = function () {
+    document.querySelector('.success').remove();
+    document.removeEventListener('keydown', onSuccessLoadEscPress);
+    document.removeEventListener('click', onSuccessLoadClickOutPress);
+  };
+
+  // Закрытие окна неуспешной загрузки
+  var closePopupErrorLoad = function () {
+    var errorButton = document.querySelector('.error__button');
+    document.querySelector('.error').remove();
+    document.removeEventListener('keydown', onErrorLoadEscPress);
+    document.removeEventListener('click', onErrorLoadClickOutPress);
+    errorButton.removeEventListener('keydown', onErrorLoadEnterPress);
+  };
+
+  // Обработчик закрытия окна при неуспешной загрузки по ESC
+  var onErrorLoadEscPress = function (evt) {
+    window.utils.isEscEvent(evt, closePopupErrorLoad);
+  };
+
+  // Обработчик закрытия окна при неуспешной загрузки по клику
+  // вне этой области экрана
+  var onErrorLoadClickOutPress = function (evt) {
+    var targetSuccess = evt.target.closest('.error__message');
+    if (!targetSuccess) {
+      closePopupErrorLoad();
+    }
+  };
+
+  // Обработчик закрытия окна при неуспешной загрузки по Enter
+  var onErrorLoadEnterPress = function (evt) {
+    window.utils.isEnterEvent(evt, closePopupErrorLoad);
+  };
+
+  // Обработчик закрытия окна успешной загрузки по ESC
+  var onSuccessLoadEscPress = function (evt) {
+    window.utils.isEscEvent(evt, closePopupSuccessLoad);
+  };
+
+  // Обработчик закрытия окна успешной загрузки по клику
+  // в этой области экрана
+  var onSuccessLoadClickOutPress = function (evt) {
+    var targetSuccess = evt.target.closest('.success__message');
+    if (!targetSuccess) {
+      closePopupSuccessLoad();
+    }
+  };
+
+  // Успешная отправка форм
+  var onLoadForm = function () {
+    window.map.resetForm();
+    // Выводит по шаблону сообщение об успешной отправки формы
+    var successTemplate = document.querySelector('#success').content;
+    var successElement = successTemplate.cloneNode(true);
+    main.appendChild(successElement);
+
+    // Слушатель на закрытие сообщения успешной отправки
+    document.addEventListener('keydown', onSuccessLoadEscPress);
+    // Слушатель по клику
+    document.addEventListener('click', onSuccessLoadClickOutPress);
+  };
+
+  // Неуспешная отправка формы
+  var onErrorForm = function () {
+    // Выводит по шаблону сообщение об неуспешной отправки формы
+    var errorTemplate = document.querySelector('#error').content;
+    var errorElement = errorTemplate.cloneNode(true);
+    main.appendChild(errorElement);
+
+    // Слушатель на закрытие сообщения успешной отправки
+    document.addEventListener('keydown', onErrorLoadEscPress);
+
+    // Слушатель по клику
+    document.addEventListener('click', onErrorLoadClickOutPress);
+
+    var errorButton = document.querySelector('.error__button');
+    // Слушатель по Enter на поле
+    errorButton.addEventListener('keydown', onErrorLoadEnterPress);
+  };
+
+  window.unloadhandlers = {
+    onLoadForm: onLoadForm,
+    onErrorForm: onErrorForm
+  };
+
+})();
