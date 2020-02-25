fb54df14d39f0b5dc984126d03c19c1c96378b8b
Georg <georgiy.nikitin@gmail.com>
2020-02-25 19:07:27 +0300

    Реализация фильтрации по типу, цене, комнатам и гостям

--- js/filter.js
+++ js/filter.js
@@ -3,9 +3,19 @@
 (function () {
   var filters = document.querySelector('.map__filters');
   var filterTypeOfHouse = filters.querySelector('#housing-type');
+  var filterPriceOfHouse = filters.querySelector('#housing-price');
+  var filterRoomsOfHouse = filters.querySelector('#housing-rooms');
+  var filterGuestOfHouse = filters.querySelector('#housing-guests');
+  // var filterFeaturesOfHouse = filters.querySelector('#housing-features');
 
   // Фильтрация по типу жилья
-  filterTypeOfHouse.addEventListener('change', function () {
+  filters.addEventListener('change', function () {
+
+    window.map.addressData.forEach(function (item) {
+      console.log(item.offer.guests);
+    });
+
+
     if (window.map.openCardStatus) {
       window.map.closePopup();
     }
@@ -18,6 +28,37 @@
       });
     }
 
+    // Фильтр по цене
+    window.map.addressDataCopy = window.map.addressDataCopy.filter(function (data) {
+      if (filterPriceOfHouse.value === 'middle' && data.offer.price >= 10000 && data.offer.price < 50000) {
+        return data.offer.price;
+      } else if (filterPriceOfHouse.value === 'low' && data.offer.price < 10000) {
+        return data.offer.price;
+      } else if (filterPriceOfHouse.value === 'high' && data.offer.price >= 50000) {
+        return data.offer.price;
+      }
+      return filterPriceOfHouse.value === 'any';
+    });
+
+    // Фильтр по числу комнат
+    window.map.addressDataCopy = window.map.addressDataCopy.filter(function (data) {
+      if (+filterRoomsOfHouse.value === +data.offer.rooms) {
+        return data.offer.rooms;
+      }
+      return filterRoomsOfHouse.value === 'any';
+    });
+
+    console.log('До фильтра guests', filterGuestOfHouse.value, window.map.addressDataCopy);
+
+    window.map.addressDataCopy = window.map.addressDataCopy.filter(function (data) {
+      console.log('Внутри guests', filterGuestOfHouse.value, data.offer.guests);
+      if (+filterGuestOfHouse.value === +data.offer.guests) {
+        return data.offer.guests;
+      }
+      return filterGuestOfHouse.value === 'any';
+    });
+
+    console.log('На выходе', window.map.addressDataCopy);
     window.pin.renderPins(window.map.addressDataCopy.slice(0, window.data.NUMBER_PIN_SHOW));
 
   });
